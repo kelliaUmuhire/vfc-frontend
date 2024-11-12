38ef1222199963db9b3b5106a365150f1e5dc045
obure1always <145106382+obure1always@users.noreply.github.com>
2024-11-12 17:13:40 +0200

    Leadership/Pastoral Team

---
+++ src/pages/Leaders.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Sermons from "./pages/Sermons";
+import Layout from "./components/layout/Layout";
+import Vision from "./pages/Vision";
+
+// Will be modified as we add more pages
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/vision",
+        element: <Vision />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/",
+        element: <Sermons />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={routes} />;
+}
