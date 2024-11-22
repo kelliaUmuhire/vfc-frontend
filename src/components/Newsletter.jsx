9811e8a07878bfc50800d2542b24d17db002eea0
kelliaUmuhire <kellumuhire@gmail.com>
2024-11-22 12:39:45 +0200

    fix

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -11,21 +11,29 @@ const Newsletter = () => {
         borderRadius: "14.222px",
       }}
     >
-      <h3 className="text-white text-xl font-bold mb-4">
-        Subscribe to our Newsletter
-      </h3>
-      <p className="text-white mb-6">
-        Stay updated with the latest news, events, and sermons.
+      <h3 className="text-lg mb-4 text-[#1B8B3B]">STAY IN THE SPIRIT</h3>
+      <p className="text-white mb-6 text-2xl">
+        <span className="font-recoleta opacity-40">
+          We would love to keep in touch.
+        </span>{" "}
+        <span className="font-recoleta">
+          Kindly subscribe to our newsletter
+        </span>
       </p>
       <form className="w-full">
         <input
+          type="text"
+          placeholder="Enter your name"
+          className="p-3 w-full mb-4 text-white rounded-xl border border-gray-400 focus:outline-none bg-transparent"
+        />
+        <input
           type="email"
           placeholder="Enter your email"
-          className="p-3 w-full mb-4 text-[#0C5220] rounded border-none focus:outline-none"
+          className="p-3 w-full mb-4 text-white rounded-xl border border-gray-400 focus:outline-none bg-transparent"
         />
         <button
           type="submit"
-          className="w-full bg-white text-[#0C5220] font-bold py-3 rounded hover:bg-gray-100 transition duration-200"
+          className="w-fit bg-secondary text-[#0C5220] font-recoleta py-2.5 px-6 uppercase rounded hover:bg-gray-100 transition duration-200"
         >
           Subscribe
         </button>
